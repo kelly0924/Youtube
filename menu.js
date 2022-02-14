1c8e72fa0c3988bd4b3a5b4b95b144c0b8f525f2
kelly0924 <hyj09240@gmail.com>
2022-02-14 11:06:02 +0000

    update Youtubu css file

--- menu.js
+++ menu.js
@@ -2,6 +2,8 @@ function showHiddenNav(){
     var mainLeftHiddenNav=document.getElementById("main_left_hidden_nav");
     if(mainLeftHiddenNav.style.display == "none"){
         mainLeftHiddenNav.style.display = "inline-block";
+       // window.getComputedStyle(mainLeftHiddenNav).width="300px"
+        window.getComputedStyle(mainLeftHiddenNav).transitionDuration="1s"; 
     }else{
         mainLeftHiddenNav.style.display="none";
     }
