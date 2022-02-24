b788482a17457bff9cb1c69eeb902df2a66914fd
kelly0924 <hyj09240@gmail.com>
2022-02-24 12:55:39 +0000

    update js file

--- menu.js
+++ menu.js
@@ -1,23 +1,25 @@
 function showHiddenNav(){
+
     var mainLeftHiddenNav=document.getElementById("main_left_hidden_nav");
     var mainCenterDiv=document.getElementsByClassName("main_center_div");
     var mainCenter=document.getElementById("main_center");
     var mainCenterDivImg=document.getElementsByClassName("main_center_div_img");
     var mainCenterDivDetails=document.getElementsByClassName("main_center_div_details");
 
-    if(mainLeftHiddenNav.style.display == "none"){
-        mainLeftHiddenNav.style.display = "inline-block";
+    if(window.getComputedStyle(mainLeftHiddenNav).display == "none"){
         document.getElementById("main_left_nav").style.display="none"
+        mainLeftHiddenNav.style.display = "block";
         for(var index=0; index <mainCenterDivImg.length; index++){
-            mainCenterDiv[index].style.width="23%";
-            mainCenterDivImg[index].style.width="250px";
-            mainCenterDivImg[index].style.height="150px";
+            mainCenterDivImg[index].style.width="260px";
+            mainCenterDivImg[index].style.height="170px";            
         }
         mainCenter.style.left="17%";
         mainCenter.style.width="85%";
-    }else{
+    }else{// 여길 먼저 들어 간다? 
+        console.log('wh?')
         mainLeftHiddenNav.style.display="none";
-        document.getElementById("main_left_nav").style.display="inline-block";
+        document.getElementById("main_left_nav").style.display="block";
+
         mainCenter.style.width="100%"
         mainCenter.style.left="0%";
         for(var index=0; index <mainCenterDivImg.length; index++){
