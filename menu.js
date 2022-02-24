588677e98627433f81687d89f5d4d9b7cc2fe5fb
kelly0924 <hyj09240@gmail.com>
2022-02-24 07:42:12 +0000

    add mobile.css file

--- menu.js
+++ menu.js
@@ -1,10 +1,29 @@
 function showHiddenNav(){
     var mainLeftHiddenNav=document.getElementById("main_left_hidden_nav");
+    var mainCenterDiv=document.getElementsByClassName("main_center_div");
+    var mainCenter=document.getElementById("main_center");
+    var mainCenterDivImg=document.getElementsByClassName("main_center_div_img");
+    var mainCenterDivDetails=document.getElementsByClassName("main_center_div_details");
+
     if(mainLeftHiddenNav.style.display == "none"){
         mainLeftHiddenNav.style.display = "inline-block";
-       // window.getComputedStyle(mainLeftHiddenNav).width="300px"
-        window.getComputedStyle(mainLeftHiddenNav).transitionDuration="1s"; 
+        document.getElementById("main_left_nav").style.display="none"
+        for(var index=0; index <mainCenterDivImg.length; index++){
+            mainCenterDiv[index].style.width="23%";
+            mainCenterDivImg[index].style.width="250px";
+            mainCenterDivImg[index].style.height="150px";
+        }
+        mainCenter.style.left="17%";
+        mainCenter.style.width="85%";
     }else{
         mainLeftHiddenNav.style.display="none";
+        document.getElementById("main_left_nav").style.display="inline-block";
+        mainCenter.style.width="100%"
+        mainCenter.style.left="0%";
+        for(var index=0; index <mainCenterDivImg.length; index++){
+            mainCenterDivImg[index].style.width="320px";
+            mainCenterDivImg[index].style.height="170px";
+
+        }
     }
 }
